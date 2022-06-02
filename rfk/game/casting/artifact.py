6d289723d904845016202a31ef917163ecdc1804
elohel <matt.rucker@gmail.com>
2022-06-01 20:25:15 -0500

    implemented setter/getter for artifact class using properties, altered main to not use a set method for the message's method

--- rfk/game/casting/artifact.py
+++ rfk/game/casting/artifact.py
@@ -1,3 +1,15 @@
 from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    def __init__(self):
+        self._message = ""
+        super().__init__()
+
+@property
+def message(self):
+    return self._message
+
+@message.setter
+def message(self, message):
+    self._message = message
